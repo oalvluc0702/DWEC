6256322c0c8c1018b15617d35e3c8008ed2d4559
Óscar Álvarez Lucas <oscarieshungria@gmail.com>
2026-09-18 14:10:50 +0200

    Añadido el docker de vite para empezar con la práctica

---
+++ practica1/mi-app-vanilla/src/main.js
@@ -0,0 +1,8 @@
+console.log("Hola Mundo");
+let numero = 5;
+for (let index = 1; index < numero; index++) {
+    if (index === 3) {
+        console.log("Ha llegado al número 3");
+    }
+ //   index = 1;
+}
