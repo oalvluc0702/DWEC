8de007d42cbae74d4b8002bff6bb8cb3300d5c4b
Óscar Álvarez Lucas <oscarieshungria@gmail.com>
2026-09-25 09:02:42 +0200

    borrado el main.js para que no tenga el menú de vite por defecto

--- practica1/mi-app-vanilla/src/main.js
+++
@@ -1,8 +0,0 @@
-console.log("Hola Mundo");
-let numero = 5;
-for (let index = 1; index < numero; index++) {
-    if (index === 3) {
-        console.log("Ha llegado al número 3");
-    }
- //   index = 1;
-}
